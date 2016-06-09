ecf7178de620a01f513e59cc8d48141fe6a02f63
adamwlev <awl44@me.com>
2016-06-09 18:58:25 -0400

    ty

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -7,7 +7,7 @@ last_names = set(i.split()[-1] for i in data.name)
 dict_q6 = {}
 for last_name in last_names:
 	for index, entry in data[[True if i.split()[-1]==last_name else False for i in data.name]].iterrows():
-		dict_q6[last_name] = dict_q6.get(last_name,[]) + [entry.degree.strip(),' '.join(entry.title.split()[:-2]),entry.email]
+		dict_q6.setdefault(last_name,[]).append([entry.degree.strip(),' '.join(entry.title.split()[:-2]),entry.email])
 
 print {key:dict_q6[key] for key in dict_q6.keys()[:3]}
 
