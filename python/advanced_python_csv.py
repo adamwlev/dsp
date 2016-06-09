d9c030f5389dac7158d95c9b9e9ca48b908b7d82
adamwlev <awl44@me.com>
2016-06-09 13:37:13 -0400

    python

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,6 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+
+data = pd.read_csv('faculty.csv')
+data.columns = [i.strip() for i in data.columns]
+
+data.email.to_csv('emails.csv',index=False)
--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,12 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+
+data = pd.read_csv('faculty.csv')
+data.columns = [i.strip() for i in data.columns]
+
+last_names = set(i.split()[-1] for i in data.name)
+dict_q6 = {}
+for last_name in last_names:
+	for index, entry in data[[True if i.split()[-1]==last_name else False for i in data.name]].iterrows():
+		dict_q6[last_name] = dict_q6.get(last_name,[]) + [entry.degree.strip(),' '.join(entry.title.split()[:-2]),entry.email]
+
+print {key:dict_q6[key] for key in dict_q6.keys()[:3]}
