cf35242b1d0e528065c8bd101f111733e7d06b5b
adamwlev <awl44@me.com>
2016-06-09 23:40:01 -0400

    up way to late

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -9,13 +9,15 @@ for last_name in last_names:
 	for index, entry in data[[True if i.split()[-1]==last_name else False for i in data.name]].iterrows():
 		dict_q6.setdefault(last_name,[]).append([entry.degree.strip(),' '.join(entry.title.split()[:-2]),entry.email])
 
-print {key:dict_q6[key] for key in dict_q6.keys()[:3]}
+print {item[0]:item[1] for u,item in enumerate(dict_q6.iteritems()) if u<3}
 
 ## check to make sure there aren't two people with the same first and last name in which case the dict comprehension would fail
 assert len(set((row['name'].split()[0],row['name'].split()[-1]) for index,row in data.iterrows()))==len([(row['name'].split()[0],row['name'].split()[-1]) for index,row in data.iterrows()])
 dict_q7 = {(row['name'].split()[0],row['name'].split()[-1]):[row['degree'].strip(),' '.join(row['title'].split()[:-2]),row['email']] for index,row in data.iterrows()}
 
-print {key:dict_q7[key] for key in dict_q7.keys()[:3]}
+
+print {item[0]:item[1] for u,item in enumerate(dict_q7.iteritems()) if u<3}
+
 
 for key in sorted(dict_q7,key=lambda x: x[1]):
 	print key,dict_q7[key]
